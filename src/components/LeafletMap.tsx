83a444f78809f8497e0d33204627fa34d68eb98c
Aditya Kakarla <adityakakarla@Adityas-MacBook-Air.local>
2024-03-05 17:41:28 -0800

    modified map

--- src/components/LeafletMap.tsx
+++ src/components/LeafletMap.tsx
@@ -5,7 +5,12 @@ import {
     useMap,
     Polygon, 
     GeoJSON,
+    Popup,
   } from 'react-leaflet';
+  import { FullscreenControl } from 'react-leaflet-fullscreen';
+  import "react-leaflet-fullscreen/styles.css"
+  import Control from 'react-leaflet-custom-control'
+  import {Download as DownloadIcon} from '@mui/icons-material'
   import { GeoJsonObject } from 'geojson'
   import mapData from "./Classify.json"
   import mapData2 from "./labels.json"
@@ -21,16 +26,6 @@ interface GeojsonStyle {
 
 export default function LeafletMap() {
     const elementRef = useRef<HTMLDivElement>(null);
-
-    const goFullScreen = () => {
-      const element = elementRef.current;
-      console.log('Full Screen')
-      if(element){
-        if(element.requestFullscreen){
-          element.requestFullscreen();
-        }
-      }
-    }
 
     const initialGeojsonStyles = [{
       show: false,
@@ -55,7 +50,12 @@ export default function LeafletMap() {
           <MapContainer
             center={[17.792094, -77.188759]}
             zoom={13}
-            style={{ width: '96%', height: '100%', marginLeft: '2%', marginRight: '2%'}}
+            style={{ width: '96%',
+                    height: '100%',
+                    marginLeft: '2%',
+                    marginRight: '2%',
+                    borderRadius: '20px',
+                    boxShadow: '4px 7px 18px 0px rgba(0,0,0,0.5)'}}
             className="map"
           >
             <GeoJSON
@@ -93,15 +93,20 @@ export default function LeafletMap() {
             />
             </BaseLayer>
           </LayersControl>
-            {/*
-            <Marker position={[51.505, -0.09]}>
-              <Popup>
-                A pretty CSS3 popup. <br /> Easily customizable.
-              </Popup>
-            </Marker> */}
+          <FullscreenControl/>
+          <Control prepend={false} position='bottomleft'>
+              <button style={{backgroundSize: '26px 26px',
+                              backgroundColor: 'white',
+                              paddingTop: '6px',
+                              paddingLeft: '7.5px',
+                              paddingRight: '7.5px',
+                              border: '2px solid rgba(0,0,0,0.25)',
+                              borderRadius: '4px',
+                              }}>
+                <DownloadIcon />
+              </button>
+          </Control>
           </MapContainer>
-          <button style={{marginLeft:"2%", fontSize:"1.5vw"}}>Download</button>
-          <button style={{fontSize:"1.5vw"}} onClick={goFullScreen}>Full Screen</button>
         </div>
         </div>
     )
